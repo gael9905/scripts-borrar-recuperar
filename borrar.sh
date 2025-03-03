f9b915125a73cae1f19c70d3dbe47f31a8986a31
gael9905 <148783238+gael9905@users.noreply.github.com>
2025-03-02 23:51:28 -0600

    Add files via upload

---
+++ borrar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+######
+#Mi primer escript en bash
+#######
+mkdir ~/.basurero 2>/dev/null
+mv $1 ~/.basurero 
---
+++ recuperar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+######
+#Mi primer escript en bash
+#######
+#El primer parametro es el nombre del archivo y el segundo la ruta donde quieres pegar
+#el archivo
+mv ~/.basurero/$1 $2
+ 
